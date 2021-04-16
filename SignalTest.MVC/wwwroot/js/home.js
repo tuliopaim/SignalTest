8e67039e9576037a37ee3b00e9464ed1942c3055
tuliopaim <tutpaim@gmail.com>
2021-04-16 10:20:36 -0300

    Add Home page

---
+++ SignalTest.MVC/wwwroot/js/home.js
@@ -0,0 +1,22 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/hub/chat").build();
+
+connection.on("HeIsHere", function (user, message) {
+    debugger;
+
+    let quantity = $("#quantity");
+
+    let pessoasOnline = parseInt(quantity.html(), 10);
+    pessoasOnline++;
+
+    quantity.html(pessoasOnline);
+});
+
+connection.start().then(function () {
+
+    connection.invoke("ImHere");
+
+}).catch(function (err) {
+    return console.error(err.toString());
+});
