35cbf0fd299d85e3efb21a36428879edac4ef297
tuliopaim <tutpaim@gmail.com>
2021-04-16 10:21:04 -0300

    Add EF

---
+++ SignalTest.MVC/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace SignalTest.MVC.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
--- SignalTest.MVC/Startup.cs
+++ SignalTest.MVC/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SignalTest.MVC.Data;
 using SignalTest.MVC.Hub;
 
 namespace SignalTest.MVC
@@ -24,6 +26,10 @@ namespace SignalTest.MVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(
+                opt => 
+                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
 
             services.AddSignalR();
