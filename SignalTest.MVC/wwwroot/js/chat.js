8d3b35c119b64668c12b7025e8ef49e843210481
tuliopaim <tutpaim@gmail.com>
2021-04-16 09:02:14 -0300

    Add project files.

---
+++ SignalTest.MVC/wwwroot/js/chat.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/hub/chat").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    debugger;
+
+    let msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let encodedMsg = user + " says " + msg;
+
+    let li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
