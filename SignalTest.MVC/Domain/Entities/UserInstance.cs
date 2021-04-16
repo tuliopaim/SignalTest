00b12953294aa53bde5c566e5c701a9493ab046e
tuliopaim <tutpaim@gmail.com>
2021-04-16 10:37:12 -0300

    Adiciona entidade

---
+++ SignalTest.MVC/Domain/Entities/UserInstance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SignalTest.MVC.Domain.Entities
+{
+    public class UserInstance
+    {
+        public UserInstance()
+        {
+            Id = Guid.NewGuid();
+            VistoPorUltimo = DateTime.Now;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime VistoPorUltimo { get; private set; }
+
+        public void Atualizar()
+        {
+            VistoPorUltimo = DateTime.Now;
+        }
+    }
+}
