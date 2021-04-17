badeb6e89bff6309a503b28f4943c213e27d2dc2
tuliopaim <tutpaim@gmail.com>
2021-04-17 13:55:14 -0300

    Cria BaseController com helpers de identidade

---
+++ SignalTest.MVC/Controllers/BaseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalTest.MVC.Controllers
+{
+    public class BaseController : Controller
+    {
+        public string ObterNomeUsuarioLogado()
+        {
+            return ObterClaim("Nome");
+        }
+
+        public Guid ObterIdUsuarioLogado()
+        {
+            var idStr = ObterClaim(ClaimTypes.NameIdentifier);
+
+            return Guid.TryParse(idStr, out var id) ? id : Guid.Empty;
+        }
+
+        public string ObterClaim(string type)
+        {
+            return User?.Claims?.FirstOrDefault(c => c.Type == type)?.Value;
+        }
+    }
+}
