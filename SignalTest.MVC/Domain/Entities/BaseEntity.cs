a4f58047b3da9a3e33c8cb8f3cf36c05554731de
tuliopaim <tutpaim@gmail.com>
2021-04-17 14:11:42 -0300

    Cria entidade base

---
+++ SignalTest.MVC/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SignalTest.MVC.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        protected BaseEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+    }
+}
