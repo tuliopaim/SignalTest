f83fa93431a6ffa536210ba0fa98c9c17e72ab6f
tuliopaim <tutpaim@gmail.com>
2021-04-17 14:12:17 -0300

    Altera o conceito de Chat para Tweet

---
+++ SignalTest.MVC/Controllers/TweetController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SignalTest.MVC.Controllers
+{
+    [Authorize]
+    public class TweetController : BaseController
+    {
+        public TweetController()
+        {
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Tweetar(string mensagem)
+        {
+            return Ok();
+        }
+    }
+}
